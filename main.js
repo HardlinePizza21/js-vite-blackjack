46cf199d241b19cba0f93450801b225683286a87
HardlinePizza21 <madridossa@gmail.com>
2025-05-07 22:36:51 -0500

    Ahora si arregalo de verdad UWU

--- main.js
+++ main.js
@@ -113,7 +113,7 @@ btnPedir.addEventListener('click', () => {
     
     // <img class="carta" src="assets/cartas/2C.png">
     const imgCarta = document.createElement('img');
-    imgCarta.src = `./assets/cartas/${ carta }.png`; //3H, JD
+    imgCarta.src = `cartas/${ carta }.png`; //3H, JD
     imgCarta.classList.add('carta');
     divCartasJugador.append( imgCarta );
 
